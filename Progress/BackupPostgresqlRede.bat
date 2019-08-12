02b3875d8aa5e6c86c523b951e74c76a74248faf
luolsuporte <suporte@luolinfo.com.br>
2019-08-12 11:44:08 -0300

    Scripts do progress alterados para funcionar em sistemas 32 e 64 bits

---
+++ Progress/BackupPostgresqlRede.bat
@@ -0,0 +1,37 @@
+@echo off
+
+rem TODAS LINHAS QUE POSSUEM "REM" NO INICIO, SERÃO IGNORADAS NA HORA DE RODAR O SCRIPT
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=C:\Arquiv~1\Postgr~1 || set OS="C:\Program Files (x86)\PostgreSQL"
+
+rem Verifica e cria diretorio de backup caso necessario
+if not exist "C:\backup\Luol\Db" md "C:\backup\Luol\Db"
+if not exist "C:\backup\Luol\Exe" md "C:\backup\Luol\Exe"
+
+rem As linha abaixo verificam e criam os diretorios de backup em diretorios de rede, é necessario preencher o nome do micro para qual será feito o backup
+if not exist "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Db" md "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Db"
+if not exist "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Exe" md "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Exe"
+
+rem (Nome do usuário do PostgreSQL que executará o script, mesmo usuario utilizado para entrar no PgAdmin)
+rem (Nome do usuario)
+set PGUSER=postgres
+rem (Senha do usuário)
+set PGPASSWORD=luol2003
+
+echo "Aguarde, realizando o backup do Banco de Dados"
+
+rem Este comando serve para armazenar a data no formato dia-mes-ano na variável Date;
+for /f "tokens=1,2,3,4 delims=/ " %%a in ('DATE /T') do set Date=%%a
+
+rem Executa o backup do banco de dados e executavel para os diretorios descritos
+%OS%\PostgreSQL\8.1\bin\vacuumdb -U postgres -d PREENCHER NOME DO BANCO DE DADOS
+%OS%\PostgreSQL\8.1\bin\pg_dump -U postgres -f "C:\backup\Luol\Db\PREENCHER NOME DO BANCO DE DADOS%Date%.out" PREENCHER NOME DO BANCO DE DADOS
+copy "C:\Progress\Progress.exe" "C:\backup\Luol\Exe\Progress%Date%.exe"
+copy "C:\Progress\Caixa.exe" "C:\backup\Luol\Exe\Caixa%Date%.exe"
+
+copy "C:\backup\Luol\Db\PREENCHER NOME DO CLIENTE%Date%.out" "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Db\PREENCHER NOME DO CLIENTE%Date%.out"
+copy "C:\Progress\Progress.exe" "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Exe\Progress%Date%.exe"
+copy "C:\Progress\Caixa.exe" "\\NOME DO MICRO DE DESTINO DO BACKUP\backup\Luol\Exe\Caixa%Date%.exe"
+
+rem (sair da tela depois do backup)
+exit
--- Progress/ConfigBackup.bat
+++ Progress/ConfigBackup.bat
@@ -7,7 +7,7 @@ If %TipoBackup%==1 GOTO BackupLocal
 If %TipoBackup%==2 GOTO BackupRede
 
 :BackupLocal
-copy BackupProgress.bat C:\Progress\BackupProgress.bat
+copy BackupPostgresql.bat C:\Progress\BackupProgress.bat
 set /p NomeTarefa=Preencha o nome da tarefa a ser criada:
 set /p HoraTarefa=Preencher com o horario do backup, (O horario deve ser preenchido no formato de 24h, ex: 8:00, 16:00, 14:30):
 schtasks /create /SC DAILY /TN %NomeTarefa% /TR C:\Progress\BackupProgress.bat /ST %HoraTarefa%
@@ -15,7 +15,7 @@ pause
 exit
 
 :BackupRede
-copy BackupProgressRede.bat C:\Progress\BackupProgress.bat
+copy BackupPostgresqlRede.bat C:\Progress\BackupProgress.bat
 set /p NomeTarefa=Preencha o nome da tarefa a ser criada:
 set /p HoraTarefa=Preencher com o horario do backup, (O horario deve ser preenchido no formato de 24h, ex: 8:00, 16:00, 14:30):
 schtasks /create /SC DAILY /TN %NomeTarefa% /TR C:\Progress\BackupProgress.bat /ST %HoraTarefa%
